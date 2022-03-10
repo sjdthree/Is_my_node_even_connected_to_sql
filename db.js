3f0edb559ff906721d800e1d893b1e53e8a75225
worthybrae <worthy@musicplace.io>
2022-03-10 12:34:34 -0500

    updated security

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
 require ('dotenv').config();
 const { Pool } = require("pg");
+const fs = require('fs');
 
 const pool = new Pool({
-  user: 'hashchatViewer',
-  database: 'hashchat',
-  password: 'Hzm8vlS9ZMKIeiVw',
-  port: 25060,
-  host: 'hashchat-do-user-9645939-0.b.db.ondigitalocean.com',
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+  host: process.env.HOST,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: true,
+    ca: fs.readFileSync('ca-certificate.crt').toString(), // make sure to drag the ca file into the same parent folder as this script
   }
 });
 
--- insertTestMessage.js
+++ insertTestMessage.js
@@ -11,7 +11,7 @@ function makeFakeWalletAddress() {
 };
 
 function makeFakeMessage() {
-  var result           = '0x';
+  var result           = '';
   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ';
   var charactersLength = characters.length;
   for ( var i = 0; i < 100; i++ ) {
